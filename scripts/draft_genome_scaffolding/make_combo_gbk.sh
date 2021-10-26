9b0b6d22cf7c552c3bf55b0c6c4ff3bc394a835e
Jeff Chang lab at Oregon State University <osu.chang@gmail.com>
2021-10-25 19:29:24 -0700

    Add files via upload

--- scripts/draft_genome_scaffolding/make_combo_gbk.sh
+++ scripts/draft_genome_scaffolding/make_combo_gbk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 for file in `ls -1 *contigorder.out`; do
 	strain=`echo -e "$file" | sed 's/Map_chromosome//g;s/_.*//g'`
-	#strain=`head -n1 $file | cut -f 2 | sed 's/_[0-9]\+$//g'`
 	python combinecontigsorder.py --fmt genbank --order $file ./draft_db/${strain}.gbk > ${strain}_reordermerge.gbk
 done
